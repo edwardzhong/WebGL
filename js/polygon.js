660887113d905b9f45410a9b86f2be58fbb038e0
edwardzhong <edwardloveyou@gmail.com>
2019-02-20 02:51:33 +0800

    update

--- js/polygon.js
+++ js/polygon.js
@@ -1,7 +1,4 @@
-	// var va = vec4(0.0, 0.0, -1.0,1);
-	// var vb = vec4(0.0, 0.942809, 0.333333, 1);
-	// var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
-	// var vd = vec4(0.816497, -0.471405, 0.333333,1);	
+;(function() {
 
 	function Polygon(position,normal,color,indices){
 		this.position=position;
@@ -275,7 +272,12 @@
         v[2] = e * g;
         return v;
     }
-
+    window.Plane=Plane;
+    window.Cube=Cube;
+    window.Sphere=Sphere;
+    window.Cylinder=Cylinder;
+    window.Cone=Cone;
+}());
 
 
 
